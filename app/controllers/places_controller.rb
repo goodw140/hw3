fcc22e8845650d027a6dc17bed7c885d3e6d1f3a
Ian Goodwin <Ian.goodwin@kellogg.northwestern.edu>
2025-02-23 21:07:14 +0000

    code to get new place functioning

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -5,14 +5,26 @@ class PlacesController < ApplicationController
 
   end
 
-  def new
-
-  end
-
   def show
     @place = Place.find_by({"id" =>params["id"]})
     @entries = Entry.where({"place_id" => @place["id"]})
 
   end
+  
+  def new
+
+  end
+
+  def create
+    @place = Place.new
+
+    @place["name"] = params["name"]
+
+    @place.save
+
+    redirect_to "/places"
+  end
+
+
 
 end
